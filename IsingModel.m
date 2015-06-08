44b973150662af7e088d1c17f45b97c06485ccc1
Paco Jain <pacojain@gmail.com>
2015-06-08 17:01:21 -0500

    Initial commit of .project, .m, and .nb files

---
+++ IsingModel.m
@@ -0,0 +1,17 @@
+(* ::Package:: *)
+
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench June, 2015 
+   Author: Paco Jain (pacojain@gmail.com)
+*)
+
+BeginPackage["IsingModel`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+End[]
+
+EndPackage[]
